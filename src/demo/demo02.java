e5873af3408ec7b375dc73c18be90fc1f6dee10e
xsm <2781340240@qq.com>
2019-07-24 09:00:14 +0800

    Initial commit

---
+++ src/demo/demo02.java
@@ -0,0 +1,16 @@
+package demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/demo2")
+public class demo02  extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        System.out.println(id);
+    }
+}
